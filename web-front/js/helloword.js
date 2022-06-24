2d0c5238b2b81055a6433da2df7b1602f4c704dc
li_heng <1033797938@qq.com>
2022-06-24 18:05:22 +0800

    练习zepto

---
+++ web-front/js/helloword.js
@@ -0,0 +1,40 @@
+// 两种写法
+// 一个是触发的动作分开写
+$(".formbutton").click(function () {
+    $(".form").toggle(2000);
+})
+
+$('#btn').on('touchstart', function(){
+    alert('hello zepto')
+})
+
+var arr = [1,2,3];
+// 数组操作
+$(function(){
+    $.each(arr , function(index , item){
+        // console.log($.isArray(arr))
+        console.log(index , item)
+    })
+    $('#dis').hide();
+    $('.box1').on('touchstart', function(){
+        $('.box1').append('<p>我是新添加的元素</p>')
+    })
+    $('.box3').on('touchstart', function () {
+        $(this).find('p').css('background', 'red');
+    })
+    $('.box2').on('touchstart', function () {
+        $('#dis').show();
+    })
+})
+
+
+// 一个是动作都包含在function里面
+
+// $(function() {
+//     $('#btn').on('touchstart', function(){
+//         alert('hello zepto')
+//     })
+//     $(".formbutton").click(function () {
+//         $(".form").toggle(2000);
+//     })
+// })
--- web-front/readme.md
+++ web-front/readme.md
@@ -5,3 +5,32 @@ https://blog.csdn.net/weixin_43285360/article/details/121359897
 index1 是zepto实验场地
 zepto是jQuery的移动版本，h5可以使用
 src是所有的zepto的插件
+
+
+### jQuery
+
+核心 $
+1. 作为函数调用（参数）
+* function(){}
+* 选择器字符串
+* DOM code
+* html字符串
+
+2. 作为对象使用（方法）
+* $.isArray()
+* $.ajax() $.get() $.post() 发送和接受请求的
+* $.each() 遍历
+
+。。。。。。
+jQuery 对象
+概念：核心函数$调用返回的就是j对象  而且还是一个伪数组
+方法：
+1. append（）
+2. find（）
+3. show（）
+。。。。。。
+
+
+zepto 
+* 以上jQuery 的概念zepto同样使用
+ 
