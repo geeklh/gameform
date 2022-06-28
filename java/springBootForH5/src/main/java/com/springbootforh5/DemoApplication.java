2c9db6affa3fa891dff8978453c05311c0bade1a
geeklh <1033797938@qq.com>
2022-06-28 23:37:31 +0800

    spring str

--- java/springBootForH5/src/main/java/com/springbootforh5/DemoApplication.java
+++ java/springBootForH5/src/main/java/com/springbootforh5/DemoApplication.java
@@ -1,9 +1,12 @@
-package com.helloworld.demo;
+package com.springbootforh5;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.springbootforh5.dao")
 public class DemoApplication {
 
     public static void main(String[] args) {
